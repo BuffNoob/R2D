fae2b1cf57ff8523ab38683c00aa1a3e46086b85
BuffNoob <156582925+BuffNoob@users.noreply.github.com>
2025-01-28 19:14:14 +0000

    Update BloxFruitKeyless.lua

--- BloxFruitKeyless.lua
+++ BloxFruitKeyless.lua
@@ -1,1 +1,18 @@
-game:GetService("Players").LocalPlayer:Kick("Permanently banned for:\n Cheating")
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "Blox Fruit Hub v1.0",
+    Text = "Script injecting...",
+    Duration = 5
+})
+
+StarterGui:SetCore("SendNotification", {
+    Title = "Blox Fruit Hub v1.0",
+    Text = "Script injected!",
+    Duration = 5
+})
+
+task.wait(2)
+
+Players.LocalPlayer:Kick("You've been banned permanently!\nBy: Anti-cheat\nReason: Suspected suspicous client behaviour")
